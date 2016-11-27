41fd6cf3e76eb6437c2081ab3f6c2ca2b445b56b
joshherkness <joshherkness@gmail.com>
2016-11-27 17:56:16 -0500

    Completed 7 and 9

---
+++ programming-exercises/3_7.py
@@ -0,0 +1,35 @@
+# Program 3.7
+
+def main():
+    possible_colors = ['red', 'blue', 'yellow']
+    # Prompt user for colors
+    c1 = input('Enter either red, blue, or yellow: ')
+    c2 = input('Enter either red, blue, or yellow: ')
+    # Ensure that the colors are valid before mixing.
+    if (c1 in possible_colors and c2 in possible_colors):
+        print(mix(c1, c2))
+    else:
+        # Error message
+        print('You fool!  You didn\'t enter valid colors.')
+
+# Function that takes in two colors, and provies the resulting
+# secondary color after mixed together.
+def mix(c1, c2):
+    if (c1 == c2):
+        return c1
+    elif (c1.lower() == 'red' or c2.lower() == 'red'):
+        if (c1.lower() == 'blue' or c2.lower() == 'blue'):
+            return 'violet'
+        elif (c1.lower() == 'yellow' or c2.lower() == 'yellow'):
+            return 'orange'
+    elif (c1.lower() == 'blue' or c2.lower() == 'blue'):
+        if (c1.lower() == 'red' or c2.lower() == 'red'):
+            return 'violet'
+        elif (c1.lower() == 'yellow' or c2.lower() == 'yellow'):
+            return 'green'
+    elif (c1.lower() == 'yellow' or c2.lower() == 'yellow'):
+        if (c1.lower() == 'red' or c2.lower() == 'red'):
+            return 'orange'
+        elif (c1.lower() == 'blue' or c2.lower() == 'blue'):
+            return 'green'
+main()
---
+++ programming-exercises/3_9.py
@@ -0,0 +1,35 @@
+# Program 3.9
+
+def main():
+    # Prompt user for pocket number.
+    pocket = int(input('Enter the number of a pocket between 0 and 36 (inclusive): '))
+
+    # Validate using a decision statement.
+    if (pocket in range(0, 37)):
+        print('The pocket is', pocket_color(pocket))
+    else:
+        print('You fool!  You didn\'t enter a number in the correct range.')
+
+# Function that returns the color of a pocket, given
+# only the pocket number.
+def pocket_color(pocket):
+    if (pocket == 0):
+        return 'green'
+    elif (pocket in range(1, 11, 2)):
+        return 'red'
+    elif (pocket in range(2, 11, 2)):
+        return 'black'
+    elif (pocket in range(11, 19, 2)):
+        return 'black'
+    elif (pocket in range(12, 19, 2)):
+        return 'red'
+    elif (pocket in range(19, 29, 2)):
+        return 'red'
+    elif (pocket in range(20, 29, 2)):
+        return 'black'
+    elif (pocket in range(29, 37, 2)):
+        return 'black'
+    elif (pocket in range(30, 37, 2)):
+        return 'red'
+    else: return None
+main()
